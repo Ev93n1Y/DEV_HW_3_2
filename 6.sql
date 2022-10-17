191261020d51c94bd13e441b88fccfb8fefeb3ad
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2022-10-17 20:56:10 +0300

    Delete 6.sql

--- 6.sql
+++
@@ -1,6 +0,0 @@
-SELECT pr.name, AVG(dev.salary)
-FROM projects AS pr
-JOIN companies_developers AS c_d ON c_d.company_id = pr.company_id
-JOIN developers AS dev ON dev.id =c_d.developer_id
-WHERE cost = (SELECT MIN(cost) FROM projects)
-GROUP BY pr.id
